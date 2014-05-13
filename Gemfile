967afb0c197e6e58481fddc7cb4ea04d1ccd9a01
DaiFukumoto-iMac <wo.horn.dai@gmail.com>
2014-05-13 16:18:01 +0900

    友達の名前を取得して表示する．

--- Gemfile
+++ Gemfile
@@ -30,6 +30,8 @@ gem 'devise'
 gem 'omniauth'
 gem 'omniauth-facebook'
 
+gem 'koala'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,12 @@
 class HomeController < ApplicationController
 	before_filter :authenticate_user!
   def index
+#  	graph = Koala::Facebook::API.new(@current_user.token)
+	### テスト用のアクセストークン
+	### FacebookAppのGraph API Explorerのアクセストークン
+	graph = Koala::Facebook::API.new(ENV['FACEBOOK_ACCESS_TOKEN'])
+  	@profile = graph.get_object('me')
+  	@friends = graph.get_connections('me', 'friends', { :locale => 'ja-jp' })	# このアプリを登録している友達を取得する．
+
   end
 end
