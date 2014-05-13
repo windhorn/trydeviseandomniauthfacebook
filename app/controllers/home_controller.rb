f655382a021b8c5d0f7913a5046d328231f99298
DaiFukumoto-iMac <wo.horn.dai@gmail.com>
2014-05-13 17:34:02 +0900

    友達の一覧表示と写真

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,9 +4,12 @@ class HomeController < ApplicationController
 #  	graph = Koala::Facebook::API.new(@current_user.token)
 	### テスト用のアクセストークン
 	### FacebookAppのGraph API Explorerのアクセストークン
-	graph = Koala::Facebook::API.new(ENV['FACEBOOK_ACCESS_TOKEN'])
-  	@profile = graph.get_object('me')
-  	@friends = graph.get_connections('me', 'friends', { :locale => 'ja-jp' })	# このアプリを登録している友達を取得する．
-
+	@graph = Koala::Facebook::API.new(ENV['FACEBOOK_ACCESS_TOKEN'])
+  	@profile = @graph.get_object('me')
+  	@friends = @graph.get_connections('me', 'friends', { :locale => 'ja-jp' })	# このアプリを登録している友達を取得する．
+  	@friends_info = []
+  	@friends.each do |friend|
+  		@friends_info << {'id'=>friend['id'],'name'=>friend['name'],'picture'=>@graph.get_picture(friend['id'],:type => 'normal')}
+  	end
   end
 end
