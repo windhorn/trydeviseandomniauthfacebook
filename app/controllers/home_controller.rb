d187dd686da0ea23c9810e0f31e6d945f6b61844
DaiFukumoto-iMac <wo.horn.dai@gmail.com>
2014-05-14 19:37:27 +0900

    パラメータから人数の数値を受け取り，それによって表示する人数を変更するようにする．

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,16 @@
 class HomeController < ApplicationController
 	before_filter :authenticate_user!
   def index
-#  	graph = Koala::Facebook::API.new(@current_user.token)
+#  	@graph = Koala::Facebook::API.new(@current_user.token)
 	### テスト用のアクセストークン
 	### FacebookAppのGraph API Explorerのアクセストークン
 	@graph = Koala::Facebook::API.new(ENV['FACEBOOK_ACCESS_TOKEN'])
   	@profile = @graph.get_object('me')
   	@friends = @graph.get_connections('me', 'friends', { :locale => 'ja-jp' })	# このアプリを登録している友達を取得する．
-    friendnum = 3   # 出力するフレンドの数
+    # パラメータから人数の数値を受け取る．
+    if params[:id]
+      friendnum = params[:id].to_i
+    end
   	@friends_info = []
     @friends.sort_by!{rand}   # 友達の情報が入っている配列をシャッフル
   	@friends.each_with_index do |friend, index|
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { :omniauth_callbacks => 'users/omniauth_callbacks'}
   get 'home', to: 'home#index', as:'user_root'
-
+  get 'home/index/:id' => 'home#index'
   get 'welcome/index'
-
 
   root 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
